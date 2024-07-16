094053c34c001ba6d99e7fa2b025c775e543a2e2
Roberto Nares <roberto.nares@gmail.com>
2024-07-15 20:44:12 -0700

    Create a new game loop and prompt user for next move

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,9 @@
 class Game
+  attr_reader :game_over
+  
   def initialize
     @board = Array.new(3) { Array.new(3, " ") }
+    @game_over = false
   end
 
   def draw_board
@@ -10,12 +13,26 @@ class Game
     end
   end
 
+  def get_validated_input(user_input)
+    number = string_to_number(user_input)
+    number = number.between?(1, 3) ? number : nil
+    number
+  end
+
   private
 
   def print_row(row)
     row.each_with_index do |token, idx|
       col_separator = idx == row.length - 1 ? "\n" : '|'
       print " #{token} #{col_separator}"
+    end
+  end
+
+  def string_to_number(string)
+    begin
+      Integer(string || '')
+    rescue ArgumentError
+      -1
     end
   end
 
--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+require_relative 'lib/game'
+
 quit_game = false
 
 puts 'Welcome to my tic, tac, toe game built with Ruby!'
@@ -13,10 +15,42 @@ until quit_game
 
   if user_input == '1'
     puts 'Starting a new game...'
+    game = Game.new
+
+    until game.game_over
+      puts
+      puts 'Current game board:'
+      game.draw_board
+
+      puts
+      puts 'Please enter your next move...'
+      puts
+      puts '  > Enter a row number between 1 and 3'
+      puts
+      print 'Your input: '
+      row = game.get_validated_input(gets.chomp)
+      puts
+
+      unless row
+        puts 'Row number is invalid, please try again...'
+        next
+      end
+
+      puts '  > Enter a column number between 1 and 3'
+      puts
+      print 'Your input: '
+      column = game.get_validated_input(gets.chomp)
+      puts
+
+      unless column
+        puts 'Column number is invalid, please try again...'
+        next
+      end
+    end
   elsif user_input == '2'
     puts 'Exiting the program...'
     quit_game = true
   else
-    puts 'Unrecognized input, please try again...'
+    puts 'Invalid input, please try again...'
   end
 end
